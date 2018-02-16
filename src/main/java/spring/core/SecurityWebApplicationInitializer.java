ee625ef1977fc45e39b6ea01967a1c3d3e4debe1
minjae-Kim <pose2done@naver.com>
2018-02-16 18:08:33 +0900

    commit for merge

---
+++ src/main/java/spring/core/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package spring.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset = UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Happy World</title>
+</head>
+<body>
+index 입니다.
+</body>
+</html>
